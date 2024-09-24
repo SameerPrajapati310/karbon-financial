678e7e760ea4589ce3c78f7ab2a20a02a85bd7e0
Sameer Prajapati <sameerprajapati336@gmail.com>
2024-09-24 21:47:28 +0530

    port issue in render

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ if __name__ == '__main__':
     # Ensure the upload folder exists
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
